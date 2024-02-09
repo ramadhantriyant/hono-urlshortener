6ab0e10dd99d06ee7a3fab25d5005e99f43c478a
Ramadhan Triyanto Prabowo <me@ramadhantriyant.id>
2024-02-09 22:59:12 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from 'hono'
+
+type Bindings = {
+  HONOLINK: KVNamespace
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', c => {
+  return c.text('Hono URL Shortener')
+})
+
+app.get('/notfound', c => {
+  return c.notFound()
+})
+
+app.get('/:shortlink', async c => {
+  const shortlink = c.req.param('shortlink')
+  const longlink = await c.env.HONOLINK.get(shortlink) || '/notfound'
+
+  return c.redirect(longlink, 302)
+})
+
+app.post('/', async c => {
+  const body = await c.req.json()
+  await c.env.HONOLINK.put(body.short, body.long)
+
+  return c.json({ status: 200 }, 200)
+})
+
+export default app
